0585edd5346b1275d0c31b56dbd840c3ea5468e5
Stray Wanderer <absocube@qq.com>
2020-04-29 12:42:39 +0800

    更新游戏框架
    删去最高分&更换人物功能
    增加游戏地图
    规范了ShowImage.py的代码

--- ShowImage.py
+++ ShowImage.py
@@ -1,10 +1,14 @@
 import pygame
 from pygame.locals import *
 pygame.init()
-class ShowImage():
+
+
+class LoadImage:
     def __init__(self, img_path):
         self.img_path = img_path
+
     def show_image(self, xy, scr):
         scr.blit(pygame.image.load(self.img_path), xy)
+
     def show_transform_image(self, xy, width, height, scr):
         scr.blit(pygame.transform.smoothscale(pygame.image.load(self.img_path), (width, height)), xy)
--- Sokaban.py
+++ Sokaban.py
@@ -2,101 +2,83 @@
 import pygame, sys, time, random
 from ClassButton import button
 from Write import Write
-from ShowImage import ShowImage
+from ShowImage import LoadImage
 from pygame.locals import *
+import numpy as np
+from Maps import *
 pygame.init()
-# create
+# 游戏初始化
 screen = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Sokaban")
+pygame.display.set_caption("PySocoban")
 pygame.display.set_icon(pygame.image.load("images/icon.ico"))
-screen.fill((255, 255, 255))
-# load imgaes
-Steve = ShowImage("images/Steve.png")
-Alex = ShowImage("images/Alex.png")
-plank = ShowImage("images/plank.png")
-case = ShowImage("images/case.png")
-TPH = ShowImage("images/TPH.png")
-bg = ShowImage("images/grassfarm.jpg")
-# get font
+# 加载资源
+Steve = LoadImage("images/Steve.png")
+plank = LoadImage("images/plank.png")
+case = LoadImage("images/case.png")
+TPH = LoadImage("images/TPH.png")
+bg = LoadImage("images/grassfarm.jpg")
+# 字体
 mcfont = pygame.font.Font("font/Mouse.ttf", 50)
 text1 = Write("Easy\tNormal\tHard\tRandom", (0, 0, 0), screen)
 choose_text = mcfont.render("Choose difficulty:", True, (0, 0, 0))
 ctrect = choose_text.get_rect()
 ctrect.centerx = 400
 ctrect.centery = 325
-# new button
+# 按钮
 pfont = pygame.font.Font("font/Mouse.ttf", 30)
-play = button(350, 450, 100, 50, "Play", pfont, (71, 1, 3), textcolor = (255, 255, 255))
-# Game conditions
+play = button(350, 450, 100, 50, "Play", pfont, (71, 1, 3), textcolor=(255, 255, 255))
+# flags
 starts = False
 chooses = True
-easy = False
-normal = False
-hard = False
-randoms = False
-plays = False
-# 'pc' point
-use_time = time.time()
+mode = ""
 # MaxScore
 file = open("MaxScore.s", "w")
 # show logo
+screen.fill((255, 255, 255))
 TPH.show_transform_image((250, 275), 300, 250, screen)
 pygame.display.update()
 time.sleep(3)
-# games
+
+# 游戏主循环
 while True:
-    """get mouse pos and game keys!"""
+    # 按键检测
     mx, my = pygame.mouse.get_pos()
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == QUIT:
-            etime = time.time() - use_time
-            etime = "%.2f" % eitme
-            file.write(etime)
-            file.close()
             sys.exit()
         if event.type == MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if not starts:
-                    if chooses:
-                        if 73 <= mx <= 186 and 375 <= my <= 425:
-                            easy = True
-                            normal = False
-                            hard = False
-                            randoms = False
-                            plays = True
-                        if 210 <= mx <= 375 and 375 <= my <= 425:
-                            easy = False
-                            normal = True
-                            hard = False
-                            randoms = False
-                            plays = True
-                        if 398 <= mx <= 513 and 375 <= 425:
-                            easy = False
-                            normal = False
-                            hard = True
-                            randoms = False
-                            plays = True
-                        if 535 <= mx <= 723 and 375 <= 425:
-                            easy = False
-                            normal = False
-                            hard = False
-                            randoms = True
-                            plays = True
-    # game not start
+            if event.button == 1 and not starts and chooses:
+                if 73 <= mx <= 186 and 375 <= my <= 425:
+                    mode = "easy"
+                if 210 <= mx <= 375 <= my <= 425:
+                    mode = "normal"
+                if 398 <= mx <= 513 and 375 <= my <= 425:
+                    mode = "hard"
+                if 535 <= mx <= 723 and 375 <= my <= 425:
+                    mode = "random"
+        if play.pressed(event) and mode:
+            starts = True
+            gmap = maps[mode]
+
+    # 游戏封面
     if not starts:
         bg.show_image((0, 0), screen)
+        play.show(screen)
         if chooses:
             text1.show_center()
             screen.blit(choose_text, ctrect)
-            if easy or normal or hard or randoms:
-                if plays:
-                    play.show(screen)
-                    if play.pressed(event):
-                        starts = True
-    # game start
+
+    # 游戏
     elif starts:
         bg.show_image((0, 0), screen)
-        plank.show_image((0, 0), screen)
-    # update game
+        for index1 in range(0, 16):
+            for index2 in range(0, 16):
+                if gmap[index1][index2] == 1:
+                    plank.show_image((index1*50, index2*50), screen)
+                elif gmap[index1][index2] == 2:
+                    case.show_image((index1*50, index2*50), screen)
+                elif gmap[index1][index2] == 3:
+                    Steve.show_image((index1*50, index2*50), screen)
+
     pygame.display.update()
